64857282aa024294b6360adc67b1ae20fafb81a5
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-07-09 15:03:07 -0700

    just updating

--- plots.js
+++ plots.js
@@ -49,6 +49,7 @@ function update(data) {
 
 d3.csv('https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv', convertRow).then(function(data) {
     update(data);
+    console.log(data);
 });
 
 
