2e4bfe1aa7ddc72ab98049ae43b059cbabab2695
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-07-12 14:46:47 -0700

    updating any changes

--- logic.js
+++ logic.js
@@ -1,42 +1,54 @@
-// so far this is just some extra practice functions that haven't worked for anything 
-// like the belly button one 
+// overview.js creates the interactive drop down overview dashboard for each hospital in our dataset. 
+
+let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
+let geoData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv'
 
 function init() {
   let dropdown = d3.select("#selDataset");
-  //create dropdown using sample ID numbers 
-  d3.csv("https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv").then((data) => {
-      console.log(data);
-      let hospitalNames = data.name;
+
+  //create dropdown using hospital names
+  d3.csv(hospData).then((data) => {
+  console.log(data);
+  let hospitalNames = data.names;
+
       //create dropdown with hospital names
-      for (let i = 0; i < hospitalNames.length; i++){
-          dropdown.append("option").text(sampleIDs[i]).property("value", sampleIDs[i]);
+      for (let i = 0; i < locationData.length; i++){
+          dropdown.append("option").text(hospitalNames[i]).property("value", hospitalNames[i]);
       };
+
       //build first plot using the first sample number. use selectSample to do the rest. 
       let firstHospital = hospitalNames[0];
       demographicInfo(firstHospital);
-
-      //createBarChart(firstSample);
-      //createBubbleChart(firstSample);
   });
 }
 
+
 // repeat for other hospital names
 function select(nextHospital) {
- // createBarChart(nextID);
-  //createBubbleChart(nextID);
   demographicInfo(nextHospital);
 }
 
+
 //create the demographics table for each hospital on dropdown menu
-function demographicInfo(hospital) {
-  d3.csv("https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv").then((data) => {
+function demographicInfo(response) {
+  d3.csv(hospData).then((data) => {
     //how to separate data by commas for each value for each row??? 
-    let metadata = data.metadata;
-    //filter data for the specific hospital 
-    let resultArray = metadata.filter(sampleObj => sampleObj.id == hospital);
+    let hospital = data.columns["Hospital Name"];
+    let city = data.columns.City; 
+    let state = data.columns.State; 
+    let type = data.columns["Hospital Type"];
+    let ownership = data.columns['Hospital Ownership'];
+    let rating = data.columns['Hospital overall rating'];
+
+    let resultArray = [hospital, city, state, type, ownership, rating];
+
+    //filter data for the specific hospital --> old code from belly button: 
+    //let resultArray = metadata.filter(sampleObj => sampleObj.id == response);
     let result = resultArray[0];
-    //select the demographic data from the dropdown with the sample ID of interest 
+
+    //select the demographic data from the dropdown with the hospital name of interest 
     let demographics = d3.select("#sample-metadata");
+
     //need this to reset the table
     demographics.html("");
 
@@ -48,7 +60,13 @@ function demographicInfo(hospital) {
 }
 
 
-//d3.csv('https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv').then(function(data;
-//});
+// Initialize the belly button dashboard
+init();
 
-
+d3.csv(geoData).then(locationInfo => {
+  d3.csv(hospData).then(hospitalInfo =>{
+    console.log(locationInfo)
+    console.log(hospitalInfo)
+    demographicInfo//(locationInfo.features)
+  });
+});
--- plots.js
+++ plots.js
@@ -1,62 +1,76 @@
 // functions to read in csv data and create visual html plots 
 let geoData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv'
-//let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
+let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
 
-d3.csv(geoData). then(function(locationinfo){
+function createMap(location){
 
-  console.log(locationinfo);
-  createFeatures(locationinfo.features);
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+   });
 
-})
+  let myMap = L.map("map", {
+     center: [37.09, -95.71],
+     zoom: 5,
+     layers: [street, location]
+  });
+
+  let baseMaps = {
+     "street Map": street
+   };
+
+  let overlayMaps = {
+     Locations: location
+   };
+
+   L.control.layers(baseMaps, overlayMaps, {
+     collapsed: false
+    }).addTo(myMap);
+
+};
 
 function createMarker(feature, latlng){
 
   return L.circleMarker(latlng, {
     radius: markerSize(feature.columns.beds),
-    //fillColor: markerColor(feature.),
+    //fillColor: markerColor(feature. INSERT RATING PATH),
     weight: 0.5,
     opacity: 0.5,
     fillOpacity: 1
  });
-
 }
+
+function createMarkers(response){
+  let hospitals = response.columns.name;
+
+  let hospitalMarkers = [];
+
+  for (let index = 0; index < name.length; index++){
+    let hospital = hospitals[index];
+
+    let hospitalMarker = L.marker([columns.latitude, columns.longitude]).bindPopup(`<h3>Name:</h3> ${feature.columns.name}<h3> Address:</h3> ${feature.columns.address}<h3> Status:</h3>${feature.columns.status}
+    ${feature.columns.status}`);
+  }
+
+};
 
 function createFeatures(locationinfo){
   
   function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>Name:</h3> ${feature.columns.name}<h3> Address:</h3> ${feature.columns.address}<h3> Status:</h3> 
-    ${feature.columns.status}}`);
+    ${feature.columns.status}`);
 }
 
   let location = L.geoJSON(locationinfo, {
       onEachFeature: onEachFeature,
       pointToLayer: createMarker
-});
-
-
-function createMap(location){
-
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-     });
-
-    let myMap = L.map("map", {
-       center: [37.09, -95.71],
-       zoom: 5,
-       layers: [street, location]
-    });
-
-    let baseMaps = {
-       "street Map": street
-     };
-
-    let overlayMaps = {
-       Locations: locations
-     };
-
-     L.control.layers(baseMaps, overlayMaps, {
-       collapsed: false
-      }).addTo(myMap);
-
+  });
+  createMap(location);
 };
-}
+
+d3.csv(geoData).then(locationInfo => {
+  d3.csv(hospData).then(hospitalInfo =>{
+    console.log(locationInfo)
+    console.log(hospitalInfo)
+    createFeatures(locationInfo.features)
+  })
+})
