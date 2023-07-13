ebcfb14b6478aa6f1b5eccc2cf2752cfe98afbc6
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-07-13 16:37:26 -0700

    semi working dashboard, copied correct plots.js over and html updates

--- logic.js
+++ logic.js
@@ -22,79 +22,50 @@ function init() {
 // repeat for other hospital names
 function select(nextHospital) {
   demographicInfo(nextHospital);
-  createBarChart(nextHospital);
+  //createBarChart(nextHospital);
 }
 
 //create bar chart of the top rated hospitals
-// function createBarChart(response) {
-//   let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv';
-//   d3.csv(hospData).then((data) => {
+function createBarChart(response) {
+  let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv';
+  d3.csv(hospData).then((data) => {
 
-//     // this all probably needs to be in a for loop or fixed when data array is made (?):
-//     let hospitals = data['Hospital Name'];
+    // this all probably needs to be in a for loop or fixed when data array is made (?):
+    let hospital_name = data['Hospital Name'];
+    let hospital_rating = data["Hospital Overall Rating"];
 
-//     let chartSpecs = [
-//       {
-//         y: hospital_rating,
-//         x: hospital_name,
-//         text: hospital_name,
-//         type: 'bar',
-//         orientation: 'h',
-//       }
-//     ];
-//     Plotly.newPlot('bar', chartSpecs, {title: "Top Rated hospitals on the West Coast", xaxis: {title: "Hospitals"}});
-//   });
-// }
+    let chartSpecs = [
+    {
+      y: hospital_rating,
+      x: hospital_name,
+      text: hospital_name,
+      type: 'bar',
+      //orientation: 'h',
+    }];
+    Plotly.newPlot('bar', chartSpecs, {title: "Top Rated hospitals on the West Coast", xaxis: {title: "Hospitals"}});
+  });
+ }
 
 //create the demographics table for each hospital on dropdown menu
 function demographicInfo(hospitalInfo) {
   let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
   d3.csv(hospData).then((data) => {
-    let hospitalInfo = []
-    for (let i = 0; i < hospitalInfo; i++) {
-      let hospital = hospitalInfo[i]
-      let selectedHospital = hospitalInfo.filter((hospital) => {
-        return hospital.name ==hospital["Hospital Name"]
-      })
-      console.log(selectedHospital);
-      let hospitalStas = d3.select("#response-hospInfo");
-      hospitalStats.append([data[i]["City"], data[i]["State"], data[i]['Hospital Type'], data[i]["Hospital Ownership"], data[i]["Hospital Overall Rating"]]);
-      //need this to reset the table
+    let hospitalInfo = [];
+    for (let i = 0; i < data.length; i++) {
+      let demographics = d3.select("#response-hospInfo");
+      hospitalInfo.push([data[i]["Hospital Name"], data[i]["City"], data[i]["State"], data[i]['Hospital Type'], data[i]["Hospital Ownership"], data[i]["Hospital Overall Rating"]]);
+      let result = hospitalInfo[0];
+      //clear previous data
       demographics.html("");
       //get values and table info to match for overivew display 
       for (r in result) {
         demographics.append("h6").text(`${r.toUpperCase()}: ${result[r]}`);
       };
     }
-      
   });
 }
+
 
 // Initialize the hospital overview table
 init();
     
-    //how to separate data by commas for each value for each row??? 
-    //let resultArray = []
-    // for (let i = 0; i < data.length; i++) {
-    //   resultArray.append([data[i]["Hospital Name"], data[i].City, data[i].State, data[i]["Hospital Type"], 
-    //   data[i]['Hospital Ownership'], data[i]['Hospital overall rating']]);
-    // };
-    // let hospital = data.columns["Hospital Name"];
-    // let city = data.columns.City; 
-    // let state = data.columns.State; 
-    // let type = data.columns["Hospital Type"];
-    // let ownership = data.columns['Hospital Ownership'];
-    // let rating = data.columns['Hospital overall rating'];
-
-
-
-
-
-
-
-
-    // const CSVToArray = (data, delimiter = ',', omitFirstRow = true) =>
-    // data
-    //   .slice(omitFirstRow ? data.indexOf('Hospital Name\nAddress') + 1 : 0)
-    //   .split('\n')
-    // .map(v => v.split(delimiter));
--- plots.js
+++ plots.js
@@ -1,76 +1,60 @@
 // functions to read in csv data and create visual html plots 
+// functions to read in csv data and create visual html plots 
 let geoData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv'
 let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
-
-function createMap(location){
-
+function createMap(locations){
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
    });
-
   let myMap = L.map("map", {
-     center: [37.09, -95.71],
+     center: [36.778259, -119.417931],
      zoom: 5,
-     layers: [street, location]
+     layers: [street, locations]
   });
-
   let baseMaps = {
      "street Map": street
    };
-
   let overlayMaps = {
-     Locations: location
+     Locations: locations
    };
-
    L.control.layers(baseMaps, overlayMaps, {
      collapsed: false
     }).addTo(myMap);
-
 };
-
-function createMarker(feature, latlng){
-
-  return L.circleMarker(latlng, {
-    radius: markerSize(feature.columns.beds),
-    //fillColor: markerColor(feature. INSERT RATING PATH),
-    weight: 0.5,
-    opacity: 0.5,
-    fillOpacity: 1
- });
-}
-
-function createMarkers(response){
-  let hospitals = response.columns.name;
-
-  let hospitalMarkers = [];
-
-  for (let index = 0; index < name.length; index++){
-    let hospital = hospitals[index];
-
-    let hospitalMarker = L.marker([columns.latitude, columns.longitude]).bindPopup(`<h3>Name:</h3> ${feature.columns.name}<h3> Address:</h3> ${feature.columns.address}<h3> Status:</h3>${feature.columns.status}
-    ${feature.columns.status}`);
+function createMarker(locationInfo, hospitalInfo){
+  let locationMarkers = []
+  for (let i = 0; i < locationInfo.length; i++) {
+    let location = locationInfo[i]
+    let selectedHospital = hospitalInfo.filter((hospital) => {
+      // console.log(location.name)
+      // console.log(hospital["Hospital Name"])
+      return location.name == hospital["Hospital Name"]
+    })
+    console.log(selectedHospital)
+    let locationMarker = L.marker([location.latitude, location.longitude])
+      .bindPopup(`<h4>Name:</h4> <h5>${location.name}</h5> <h4>Address:</h4> <h5>${location.address}</h5> 
+      <h4>Rating:</h4> <h5>${selectedHospital[0]["Hospital overall rating"]}</h5>`)
+    
+    locationMarkers.push(locationMarker)
   }
-
+  createMap(L.layerGroup(locationMarkers))
 };
-
-function createFeatures(locationinfo){
+// function createFeatures(locationInfo, hospitalInfo){
   
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>Name:</h3> ${feature.columns.name}<h3> Address:</h3> ${feature.columns.address}<h3> Status:</h3> 
-    ${feature.columns.status}`);
-}
-
-  let location = L.geoJSON(locationinfo, {
-      onEachFeature: onEachFeature,
-      pointToLayer: createMarker
-  });
-  createMap(location);
-};
-
+//   function onEachFeature(feature, layer) {
+//     layer.bindPopup(`<h3>Name:</h3> ${hospitalInfo.columns.name}<h3> Address:</h3> ${feature.columns.address}<h3> Status:</h3> 
+//     ${hospitalInfo.columns["Hospital overall rating"]}`);
+// }
+//   let location = L.geoJSON(locationInfo, {
+//       onEachFeature: onEachFeature,
+//       pointToLayer: createMarker
+//   });
+//   createMap(location);
+// };
 d3.csv(geoData).then(locationInfo => {
   d3.csv(hospData).then(hospitalInfo =>{
-    console.log(locationInfo)
-    console.log(hospitalInfo)
-    createFeatures(locationInfo.features)
+    // console.log(locationInfo)
+    // console.log(hospitalInfo)
+    createMarker(locationInfo, hospitalInfo)
   })
 })
