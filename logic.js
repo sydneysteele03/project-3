22e0802e5833d700fe8f11f42fc48fb606af9f82
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-07-13 19:13:17 -0700

    working overview chart

--- logic.js
+++ logic.js
@@ -13,7 +13,7 @@ function init() {
         dropdown.append("option").text(data[i]["Hospital Name"]).property("value", data[i]["Hospital Name"]);
     };
 
-    let firstHospital = data[0];
+    let firstHospital = data[0]["Hospital Name"];
     demographicInfo(firstHospital);
     createBarChart(firstHospital);
   });
@@ -26,23 +26,31 @@ function select(nextHospital) {
 }
 
 //create the demographics table for each hospital on dropdown menu
-function demographicInfo(hospitalInfo) {
+function demographicInfo(hospitalName) {
   let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
   let geoData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv'
   d3.csv(hospData).then((data) => {
     d3.csv(geoData).then((geo) => {
-      let hospitalInfo = [];
-      for (let i = 0; i < data.length; i++) {
-        let demographics = d3.select("#response-hospInfo");
-        hospitalInfo.push([data[i]["Hospital Name"], data[i]["City"], data[i]["State"], data[i]["Hospital overall rating"], data[i]['Hospital Type'], geo[i]['beds'], data[i]["Hospital Ownership"], geo[i]['helipad']]);
-        let result = hospitalInfo[i];
-        //clear previous data
-        demographics.html("");
-        //get values and table info to match for overivew display 
-        for (r in result) {
-          demographics.append("h6").text(`${r.toUpperCase()}: ${result[r]}`);
-        };
-      }
+      let hospResult = data.filter((hospital) => {
+        return hospitalName == hospital['Hospital Name']
+      })
+      let geoResult = geo.filter((hospital) => {
+        return hospitalName ==hospital.name
+      })
+
+      let demographics = d3.select("#response-hospInfo");
+      //clear table after reselction
+      demographics.html("");
+      // add the hospital info
+      demographics.append("h4").text(`Hospital Name: ${geoResult[0].name}`);
+      demographics.append("h4").text(`City: ${geoResult[0].city}`);
+      demographics.append("h4").text(`State: ${geoResult[0].state}`);
+      demographics.append("h4").text(`Overall Rating Score: ${hospResult[0]["Hospital overall rating"]}`);
+      demographics.append("h4").text(`Hospital Type: ${geoResult[0].type}`);
+      demographics.append("h4").text(`Ownership: ${geoResult[0].owner}`);
+      demographics.append("h4").text(`Number of Beds: ${geoResult[0].beds}`);
+      demographics.append("h4").text(`Helipad Available?: ${geoResult[0].helipad}`);
+      
     });
 });
 }
