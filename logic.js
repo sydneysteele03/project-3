773d432669226f29fb6ab5162a7cda00f6082f86
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-07-13 18:42:45 -0700

    thurdsay updates

--- logic.js
+++ logic.js
@@ -22,49 +22,54 @@ function init() {
 // repeat for other hospital names
 function select(nextHospital) {
   demographicInfo(nextHospital);
-  //createBarChart(nextHospital);
+  createBarChart(nextHospital);
+}
+
+//create the demographics table for each hospital on dropdown menu
+function demographicInfo(hospitalInfo) {
+  let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
+  let geoData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv'
+  d3.csv(hospData).then((data) => {
+    d3.csv(geoData).then((geo) => {
+      let hospitalInfo = [];
+      for (let i = 0; i < data.length; i++) {
+        let demographics = d3.select("#response-hospInfo");
+        hospitalInfo.push([data[i]["Hospital Name"], data[i]["City"], data[i]["State"], data[i]["Hospital overall rating"], data[i]['Hospital Type'], geo[i]['beds'], data[i]["Hospital Ownership"], geo[i]['helipad']]);
+        let result = hospitalInfo[i];
+        //clear previous data
+        demographics.html("");
+        //get values and table info to match for overivew display 
+        for (r in result) {
+          demographics.append("h6").text(`${r.toUpperCase()}: ${result[r]}`);
+        };
+      }
+    });
+});
 }
 
 //create bar chart of the top rated hospitals
 function createBarChart(response) {
   let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv';
+  let geoData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_loc_df.csv'
   d3.csv(hospData).then((data) => {
+    d3.csv(geoData).then((data) => {
+    // set bar chart info
+      let hospital_name = data['Hospital Name'];
+      let hospital_rating = data["Hospital Overall Rating"];
 
-    // this all probably needs to be in a for loop or fixed when data array is made (?):
-    let hospital_name = data['Hospital Name'];
-    let hospital_rating = data["Hospital Overall Rating"];
-
-    let chartSpecs = [
-    {
-      y: hospital_rating,
-      x: hospital_name,
-      text: hospital_name,
-      type: 'bar',
-      //orientation: 'h',
-    }];
-    Plotly.newPlot('bar', chartSpecs, {title: "Top Rated hospitals on the West Coast", xaxis: {title: "Hospitals"}});
+      let chartSpecs = [
+      {
+        y: hospital_rating,
+        x: hospital_name,
+        text: hospital_rating,
+        type: 'bar',
+        orientation: 'h',
+      }];
+      //make the plot 
+      Plotly.newPlot('bar', chartSpecs, {title: "Top Rated hospitals on the West Coast", xaxis: {title: "Hospitals"}});
+  });
   });
  }
-
-//create the demographics table for each hospital on dropdown menu
-function demographicInfo(hospitalInfo) {
-  let hospData = 'https://raw.githubusercontent.com/sydneysteele03/project-3/main/westcoast_info_df.csv'
-  d3.csv(hospData).then((data) => {
-    let hospitalInfo = [];
-    for (let i = 0; i < data.length; i++) {
-      let demographics = d3.select("#response-hospInfo");
-      hospitalInfo.push([data[i]["Hospital Name"], data[i]["City"], data[i]["State"], data[i]['Hospital Type'], data[i]["Hospital Ownership"], data[i]["Hospital Overall Rating"]]);
-      let result = hospitalInfo[0];
-      //clear previous data
-      demographics.html("");
-      //get values and table info to match for overivew display 
-      for (r in result) {
-        demographics.append("h6").text(`${r.toUpperCase()}: ${result[r]}`);
-      };
-    }
-  });
-}
-
 
 // Initialize the hospital overview table
 init();
